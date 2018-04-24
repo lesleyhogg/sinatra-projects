128f9d5b4ad5a1ccc3a3073ca802e34da2406192
Lesley Hogg <lesleyhogg@gmail.com>
2018-04-24 13:37:27 -0400

    Finishing touches on the numerology app

--- sinatra-mvc/app/models/person.rb
+++ sinatra-mvc/app/models/person.rb
@@ -1,12 +1,12 @@
 class Person < ActiveRecord::Base
-
+  validates_presence_of :first_name, :last_name, :birthdate
   def self.get_birth_path_num(birthdate)
     number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
 
     number = number.to_s
     number = number[0].to_i + number[1].to_i
 
-    if number > 9 
+    if number > 9
       number = number.to_s
       number = number[0].to_i + number[1].to_i
     end
